e9a01b8ad83ddfe5f376d03fda1b835257d25dd7
syw8861 <syw8861@gmail.com>
2020-09-20 15:55:31 +0900

    asd

---
+++ teamProject/src/main/java/main/mainsearchResultServ.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.Controller;
+
+public class mainsearchResultServ implements Controller
+{
+
+	public void execute(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+		
+
+	}
+
+}
---
+++ teamProject/src/main/java/moneyManage/moneyInListServ.java
@@ -0,0 +1,21 @@
+package moneyManage;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.Controller;
+
+public class moneyInListServ implements Controller
+{
+
+	public void execute(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
