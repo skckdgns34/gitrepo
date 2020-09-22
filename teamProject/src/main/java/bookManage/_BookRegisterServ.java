5f99dfcf66dd2d13fdcdce38112ff51667805f5e
admin <admin@YD03-24>
2020-09-22 13:55:45 +0900

    asd

--- teamProject/src/main/java/bookManage/_BookRegisterServ.java
+++ teamProject/src/main/java/bookManage/_BookRegisterServ.java
@@ -8,11 +8,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import common.Controller;
 
-public class bookRegisterServ implements Controller {
+public class _BookRegisterServ implements Controller {
 
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-
+  
 	}
 
 }
