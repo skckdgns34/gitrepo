5ecb091ce10cfd1138d7baf67567077a7d54a42b
'son' <syw8861@gmail.com>
2020-09-23 18:04:33 +0900

    vn

--- teamProject/src/main/java/ebook/EBookSearchAfterServ.java
+++ teamProject/src/main/java/ebook/EBookSearchAfterServ.java
@@ -19,31 +19,33 @@ public class EBookSearchAfterServ implements Controller {
 		
 		response.setContentType("text/html; charset=UTF-8");
 		MainDAO dao = new MainDAO();
-		String AutoResult = request.getParameter("search");
-		
-				
+		String AutoResult = request.getParameter("hidden");
+		String AutoSearchResult = request.getParameter("realHidden");
+		System.out.println(AutoResult+"오토변수값");
+		System.out.println(AutoSearchResult+"검색값");
  		String writer = "writer";
 		String book = "book";
 		String company = "company";
 		if(AutoResult.equals(book)){
 			System.out.println("책임");
-			List<Map<String, Object>> list =dao.searchBooksEqualTitle(AutoResult);
+			List<Map<String, Object>> list = dao.searchBooksEqualTitle(AutoSearchResult);
 			request.setAttribute("list", list);
+			System.out.println(list+"책");
 		}
 		else if(AutoResult.equals(writer)) {
 			System.out.println("저자임");
-			List<Map<String, Object>> list =dao.searchBooksEqualWriter(AutoResult);
+			List<Map<String, Object>> list = dao.searchBooksEqualWriter(AutoSearchResult);
 			request.setAttribute("list", list);
+			System.out.println(list+"저자");
 		}
 		else if(AutoResult.equals(company)) {
 			System.out.println("회사이름임");
-			List<Map<String, Object>> list =dao.searchBooksEqualCompany(AutoResult);
+			List<Map<String, Object>> list = dao.searchBooksEqualCompany(AutoSearchResult);
 			request.setAttribute("list", list);
+			System.out.println(list+"회사");
 		}
 		
 		request.getRequestDispatcher("/ebook/eBookSearchAfter.jsp").forward(request, response);
-		
-		System.out.println(AutoResult);
 
 	}
 
--- teamProject/src/main/webapp/main/mainsearchResult.jsp
+++ teamProject/src/main/webapp/main/mainsearchResult.jsp
@@ -23,6 +23,7 @@
 										success : function(data) { // 성공
 											response($.map(data, function(item) {
 													$('#hidden').val(item.result);
+													$('#realHidden').val(item.title);
 													return {
 														label : item.title, //목록에 표시되는 값
 														value : item.title, //선택 시 input창에 표시되는 값	
@@ -62,7 +63,8 @@
 <body>
 		<input type="text" id="testInput" placeholder="검색할 내용 입력" name="search"/>
 	<form action="${pageContext.request.contextPath}/eBookSearchAfter.do" method="post">
-		<input type="hidden" id="hidden"  name="search" />
+		<input type="hidden" id="hidden"  name="hidden" />
+		<input type="hidden" id="realHidden"  name="realHidden" />
 		<button>검색</button>
 	</form>
 </body>
