55fc6cac11851e52ddf1b03c949d0da5080bdf6b
'son' <syw8861@gmail.com>
2020-09-23 15:56:20 +0900

    faf

--- teamProject/src/main/java/ebook/EBookCategoryServ.java
+++ teamProject/src/main/java/ebook/EBookCategoryServ.java
@@ -40,35 +40,6 @@ public class EBookCategoryServ implements Controller
 		request.setAttribute("books", books);
 		request.getRequestDispatcher("/ebook/eBookCategory.jsp").forward(request, response);
 		
-		
-		
-		response.setContentType("text/html; charset=UTF-8");
-		MainDAO dao = new MainDAO();
-		String AutoResult = request.getParameter("search");
-		
-				
- 		String writer = "writer";
-		String book = "book";
-		String company = "company";
-		if(AutoResult.equals(book)){
-			System.out.println("책임");
-			List<Map<String, Object>> list =dao.searchBooksEqualTitle(AutoResult);
-			request.setAttribute("list", list);
-		}
-		else if(AutoResult.equals(writer)) {
-			System.out.println("저자임");
-			List<Map<String, Object>> list =dao.searchBooksEqualWriter(AutoResult);
-			request.setAttribute("list", list);
-		}
-		else if(AutoResult.equals(company)) {
-			System.out.println("회사이름임");
-			List<Map<String, Object>> list =dao.searchBooksEqualCompany(AutoResult);
-			request.setAttribute("list", list);
-		}
-		
-		request.getRequestDispatcher("/main/main.jsp").forward(request, response);
-		
-		System.out.println(AutoResult);
 	}
 
 }
