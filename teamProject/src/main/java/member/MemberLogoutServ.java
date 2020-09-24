5e68290968b6223afffa3a32525722395109d38a
'monya-9' <'lee99714@naver.com'>
2020-09-24 13:58:46 +0900

    asdf

---
+++ teamProject/src/main/java/member/MemberLogoutServ.java
@@ -0,0 +1,18 @@
+package member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.Controller;
+
+public class MemberLogoutServ implements Controller {
+
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect(request.getContextPath()+"/main.do");
+
+	}
+
+}
