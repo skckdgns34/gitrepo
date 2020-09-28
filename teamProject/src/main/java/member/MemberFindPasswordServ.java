0aba069f76ac3c125c8cacb366785a3728b0daa4
'monya-9' <'lee99714@naver.com'>
2020-09-28 15:12:37 +0900

    dsvSV

--- teamProject/src/main/java/member/MemberFindPasswordServ.java
+++ teamProject/src/main/java/member/MemberFindPasswordServ.java
@@ -12,6 +12,15 @@ public class MemberFindPasswordServ implements Controller {
 
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	}
+		String AuthenticationKey = (String) request.getSession().getAttribute("AuthenticationKey");
+		String AuthenticationUser = request.getParameter("AuthenticationUser");
+		if (!AuthenticationKey.equals(AuthenticationUser)) {
+			System.out.println("인증번호 일치하지 않음");
+			request.setAttribute("msg", "인증번호가 일치하지 않습니다");
+			request.setAttribute("loc", "member/memberPassword.jsp");
+			request.getRequestDispatcher("member/memberPassword.jsp").forward(request, response);
+			return;
+		}
 
+	}
 }
