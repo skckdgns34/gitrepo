ac233fb96c97a784681a81df114024465ec842f4
'monya-9' <'lee99714@naver.com'>
2020-09-29 16:21:42 +0900

    aergaerg

--- teamProject/src/main/java/member/MemberDeleteServ.java
+++ teamProject/src/main/java/member/MemberDeleteServ.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import common.Controller;
+import vo.Member;
 
 public class MemberDeleteServ implements Controller
 {
@@ -14,7 +16,17 @@ public class MemberDeleteServ implements Controller
 	public void execute(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException
 	{
-		request.getRequestDispatcher("member/memberDelete.jsp").forward(request, response);
+		
+		HttpSession session = request.getSession();
+		
+		Member memberVO = new Member();
+		memberVO.setMember_id(request.getParameter("member_id"));
+		
+		MemberDAO dao = new MemberDAO();
+		dao.delete(memberVO);
+		
+		
+		request.getRequestDispatcher("member/Bye.jsp").forward(request, response);
 	}
 
 }
