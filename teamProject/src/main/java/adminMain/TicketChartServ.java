3b8a88eddeacd3bf399d1e74152d26f4b94e06dc
chaebin <ravis_toy@naver.com>
2020-10-05 14:10:04 +0900

    채빈

--- teamProject/src/main/java/adminMain/TicketChartServ.java
+++ teamProject/src/main/java/adminMain/TicketChartServ.java
@@ -16,10 +16,11 @@ public class TicketChartServ implements Controller {
 
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
-		List<HashMap<String, Object>> list =  MemberManageDAO.getinstance().getTicketCnt();
+		MemberManageDAO dao = new MemberManageDAO();
+		List<HashMap<String, Object>> list =  dao.getTicketCnt();
 		
 		String result = JSONArray.fromObject(list).toString();
-		System.out.println(result);
+		System.out.println(list);
 		response.getWriter().print(result);
 
 	}
