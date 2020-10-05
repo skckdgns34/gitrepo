a72e8a0ab31e69f8009b93a65c8a6a137384db48
'khb3661' <'khb3661@naver.com'>
2020-10-05 15:26:58 +0900

    as

---
+++ teamProject/src/main/java/bookManage/BookManageServ.java
@@ -0,0 +1,17 @@
+package bookManage;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.Controller;
+
+public class BookManageServ implements Controller {
+
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("bookManage/bookRegister.jsp").forward(request, response);
+	}
+
+}
