8d5d8f21ae1eb1b09e665dbdce7e7799573fc968
'khb3661' <'khb3661@naver.com'>
2020-10-06 11:04:11 +0900

    aaaa

---
+++ teamProjectAdmin/src/main/java/com/books/mapper/BooksService.java
@@ -0,0 +1,21 @@
+package com.books.mapper;
+
+import java.util.ArrayList;
+
+import com.books.Books;
+
+public interface BooksService {
+	//매입입력
+	Books insertpur (Books books);
+	//도서입력
+	public int insert (Books books);
+	//도서삭제
+	public int delete(Books book);
+	//도서선택
+	public Books selectOne(Books book);
+	//도서전체조회
+	public ArrayList<Books> selectAll(String search_text, String search_type);
+	//도서정보업데이트
+	public Books update(Books books);
+	
+}
