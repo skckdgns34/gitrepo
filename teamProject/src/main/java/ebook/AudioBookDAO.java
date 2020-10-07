c62f82d0acc670f8176f375b7031a67a844ebe09
skckdgns34 <skckdgns34@naver.com>
2020-10-07 10:39:01 +0900

    a

--- teamProject/src/main/java/ebook/AudioBookDAO.java
+++ teamProject/src/main/java/ebook/AudioBookDAO.java
@@ -20,21 +20,111 @@ public class AudioBookDAO {
 
 	static AudioBookDAO instance;
 
-	public static AudioBookDAO  getInstance() {
+	public static AudioBookDAO getInstance() {
 		if (instance == null)
 			instance = new AudioBookDAO();
 		return instance;
 	}
+
+	// ebook category 체크박스 전체 뿌려주기용
+	public ArrayList<Books> raidoAllBooks(int first, int last) {
+		ResultSet rs = null;
+		ArrayList<Books> list = new ArrayList<Books>();
+		try {
+			conn = ConnectionManager.getConnnect();
+			String sql = "select a.* from ( select rownum rn, b.* from (  "
+					+ "select title, book_no, book_img from books where audio_path is not null order by book_no desc "
+					+ ") b ) a where rn  between ? and ? ";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, first);
+			pstmt.setInt(2, last);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Books books = new Books();
+				books.setTitle(rs.getString("title"));
+				books.setBook_no(rs.getString("book_no"));
+				books.setBook_img(rs.getString("book_img"));
+				list.add(books);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionManager.close(rs, pstmt, conn);
+		}
+		return list;
+	}
+
+	// eBook Category Ajax 장르라디오 체크시
+		public ArrayList<Books> radioCheckGenre(String gen, int first, int last)
+		{
+			ArrayList<Books> list = new ArrayList<Books>();
+			ResultSet rs = null;
+
+			try
+			{
+				conn = ConnectionManager.getConnnect();
+				String sql = "select a.* from ( select rownum rn, b.* from (  " + 
+						"select title, book_no, book_img from books where genre =? and audio_path is not null order by book_no desc "
+						+ ") b ) a where rn  between ? and ? "; 
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, gen);
+				pstmt.setInt(2, first);
+				pstmt.setInt(3, last);
+				rs = pstmt.executeQuery();
+				while (rs.next())
+				{
+					Books books = new Books();
+					books.setTitle(rs.getString("title"));
+					books.setBook_no(rs.getString("book_no"));
+					books.setBook_img(rs.getString("book_img"));
+					list.add(books);
+				}
+			} catch (Exception e)
+			{
+				e.printStackTrace();
+			} finally
+			{
+				ConnectionManager.close(rs, pstmt, conn);
+			}
+			return list;
+		}
 	
-	//오디오북 검색
+	// 베스트셀러인거만 가져오기
+	public ArrayList<Books> selectBestBooks() {
+		ArrayList<Books> list = new ArrayList<Books>();
+		ResultSet rs = null;
+		try {
+			conn = ConnectionManager.getConnnect();
+			String sql = "select book_no, title, book_img from books where best_book='y' and rownum<6 and audio_path is not null order by book_no desc";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Books books = new Books();
+				books.setBook_no(rs.getString(1));
+				books.setTitle(rs.getString(2));
+				books.setBook_img(rs.getString(3));
+				list.add(books);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionManager.close(rs, pstmt, conn);
+		}
+		return list;
+	}
+
+	// 오디오북 검색
 	public ArrayList<SearchBook> searchBook(String a) {
 		ArrayList<SearchBook> list = new ArrayList<SearchBook>();
 		try {
 			SearchBook aa = null;
 			conn = ConnectionManager.getConnnect();
-			
-			String sql = "select 'book', title from books where title like '%' || ? || '%' and audio_path is not null " + " union all "
-					+ " select 'writer', writer from books where  writer like '%' || ? || '%' and audio_path is not null " + " union all "
+
+			String sql = "select 'book', title from books where title like '%' || ? || '%' and audio_path is not null "
+					+ " union all "
+					+ " select 'writer', writer from books where  writer like '%' || ? || '%' and audio_path is not null "
+					+ " union all "
 					+ " select 'company', company_name from company c, books b where  company_name like '%' || ? || '%' and b.audio_path is not null";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, a);
@@ -56,38 +146,31 @@ public class AudioBookDAO {
 		}
 		return list;
 	}
-	
-	
-	
-
 
 	public List<Map<String, Object>> searchBooksEqualTitle(String a) {
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		try {
 			conn = ConnectionManager.getConnnect();
-			String sql ="select book_no, title, writer, publication_date, "
-						+ " book_img, company_name, introduction, summary, "
-						+ " views,score ,genre "
-						+ " from books b, company c "
-						+ " where b.company_code = c.company_code "
-						+ " and b.title= ? "
-						+ " and b.audio_path is not null";
+			String sql = "select book_no, title, writer, publication_date, "
+					+ " book_img, company_name, introduction, summary, " + " views,score ,genre "
+					+ " from books b, company c " + " where b.company_code = c.company_code " + " and b.title= ? "
+					+ " and b.audio_path is not null";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, a);
 			rs = pstmt.executeQuery();
 			while (rs.next()) {
 				Map<String, Object> map = new HashMap<String, Object>();
-				map.put("book_no",rs.getString("book_no"));
-				map.put("title",rs.getString("title"));
-				map.put("writer",rs.getString("writer"));
-				map.put("publication_date",rs.getString("publication_date"));
-				map.put("book_img",rs.getString("book_img"));
-				map.put("company_name",rs.getString("company_name"));
-				map.put("introduction",rs.getString("introduction"));
-				map.put("summary",rs.getString("summary"));
-				map.put("views",rs.getString("views"));
-				map.put("score",rs.getString("score"));
-				map.put("genre",rs.getString("genre"));
+				map.put("book_no", rs.getString("book_no"));
+				map.put("title", rs.getString("title"));
+				map.put("writer", rs.getString("writer"));
+				map.put("publication_date", rs.getString("publication_date"));
+				map.put("book_img", rs.getString("book_img"));
+				map.put("company_name", rs.getString("company_name"));
+				map.put("introduction", rs.getString("introduction"));
+				map.put("summary", rs.getString("summary"));
+				map.put("views", rs.getString("views"));
+				map.put("score", rs.getString("score"));
+				map.put("genre", rs.getString("genre"));
 				list.add(map);
 			}
 		} catch (Exception e) {
@@ -97,34 +180,31 @@ public class AudioBookDAO {
 		}
 		return list;
 	}
-	
+
 	public List<Map<String, Object>> searchBooksEqualCompany(String a) {
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		try {
 			conn = ConnectionManager.getConnnect();
-			String sql ="select book_no, title, writer, publication_date, "
-						+ " book_img, company_name, introduction, summary, "
-						+ " views,score ,genre "
-						+ " from books b, company c "
-						+ " where b.company_code = c.company_code "
-						+ " and c.company_name= ?"
-						+ "	and b.audio_path is not null ";
+			String sql = "select book_no, title, writer, publication_date, "
+					+ " book_img, company_name, introduction, summary, " + " views,score ,genre "
+					+ " from books b, company c " + " where b.company_code = c.company_code " + " and c.company_name= ?"
+					+ "	and b.audio_path is not null ";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, a);
 			rs = pstmt.executeQuery();
 			while (rs.next()) {
 				Map<String, Object> map = new HashMap<String, Object>();
-				map.put("book_no",rs.getString("book_no"));
-				map.put("title",rs.getString("title"));
-				map.put("writer",rs.getString("writer"));
-				map.put("publication_date",rs.getString("publication_date"));
-				map.put("book_img",rs.getString("book_img"));
-				map.put("company_name",rs.getString("company_name"));
-				map.put("introduction",rs.getString("introduction"));
-				map.put("summary",rs.getString("summary"));
-				map.put("views",rs.getString("views"));
-				map.put("score",rs.getString("score"));
-				map.put("genre",rs.getString("genre"));
+				map.put("book_no", rs.getString("book_no"));
+				map.put("title", rs.getString("title"));
+				map.put("writer", rs.getString("writer"));
+				map.put("publication_date", rs.getString("publication_date"));
+				map.put("book_img", rs.getString("book_img"));
+				map.put("company_name", rs.getString("company_name"));
+				map.put("introduction", rs.getString("introduction"));
+				map.put("summary", rs.getString("summary"));
+				map.put("views", rs.getString("views"));
+				map.put("score", rs.getString("score"));
+				map.put("genre", rs.getString("genre"));
 				list.add(map);
 			}
 		} catch (Exception e) {
@@ -134,34 +214,31 @@ public class AudioBookDAO {
 		}
 		return list;
 	}
-	
+
 	public List<Map<String, Object>> searchBooksEqualWriter(String a) {
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		try {
 			conn = ConnectionManager.getConnnect();
-			String sql ="select book_no, title, writer, publication_date, "
-						+ " book_img, company_name, introduction, summary, "
-						+ " views,score ,genre "
-						+ " from books b, company c "
-						+ " where b.company_code = c.company_code "
-						+ " and b.writer= ?"
-						+ " and b.audio_path is not null ";
+			String sql = "select book_no, title, writer, publication_date, "
+					+ " book_img, company_name, introduction, summary, " + " views,score ,genre "
+					+ " from books b, company c " + " where b.company_code = c.company_code " + " and b.writer= ?"
+					+ " and b.audio_path is not null ";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, a);
 			rs = pstmt.executeQuery();
 			while (rs.next()) {
 				Map<String, Object> map = new HashMap<String, Object>();
-				map.put("book_no",rs.getString("book_no"));
-				map.put("title",rs.getString("title"));
-				map.put("writer",rs.getString("writer"));
-				map.put("publication_date",rs.getString("publication_date"));
-				map.put("book_img",rs.getString("book_img"));
-				map.put("company_name",rs.getString("company_name"));
-				map.put("introduction",rs.getString("introduction"));
-				map.put("summary",rs.getString("summary"));
-				map.put("views",rs.getString("views"));
-				map.put("score",rs.getString("score"));
-				map.put("genre",rs.getString("genre"));
+				map.put("book_no", rs.getString("book_no"));
+				map.put("title", rs.getString("title"));
+				map.put("writer", rs.getString("writer"));
+				map.put("publication_date", rs.getString("publication_date"));
+				map.put("book_img", rs.getString("book_img"));
+				map.put("company_name", rs.getString("company_name"));
+				map.put("introduction", rs.getString("introduction"));
+				map.put("summary", rs.getString("summary"));
+				map.put("views", rs.getString("views"));
+				map.put("score", rs.getString("score"));
+				map.put("genre", rs.getString("genre"));
 				list.add(map);
 			}
 		} catch (Exception e) {
--- teamProject/src/main/webapp/ebook/audioBookCategory.jsp
+++ teamProject/src/main/webapp/ebook/audioBookCategory.jsp
@@ -10,72 +10,82 @@
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<link rel="stylesheet" href="${pageContext.request.contextPath}/layout/styles/slider.css">
 
 <script>
-	$(function() {
-		$("#testInput").autocomplete({
-							source : function(request, response) {
-								$.ajax({
-										url : "${pageContext.request.contextPath}/Ajax/audioBookSearchAjax.do",
-										type : "GET",
-										dataType : "json",
-										data : {data : $("#testInput").val()}, // 검색 키워드
-										success : function(data) { // 성공
-											response($.map(data, function(item) {
-													$('#hidden').val(item.result);
-													$('#realHidden').val(item.title);
-													return {
-														label : item.title, //목록에 표시되는 값
-														value : item.title, //선택 시 input창에 표시되는 값	
-														idx : item.testIdx
-													};
-											})); //response
-										},//success
-										error : function() { //실패
-											alert("통신에 실패했습니다.");
-											}
-										});//ajax
-							},//source: function
-							minLength : 1,
-							autoFocus : false,
-							select : function(evt, ui) {
-								//$('#hidden').html(ui.item.value);
-								//var v = ui.item.label;
-								//var arr = v.split('/');
-								//$('#testInput').val(arr[0]);
-								//$('#hidden').val(arr[1]);
-								console.log("전체 data: " + JSON.stringify(ui));
-								console.log("db Index : " + ui.item.idx);
-								console.log("검색 데이터 : " + ui.item.value);
-							},
-							focus : function(evt, ui) {
-								return false;
-							},
-							close : function(evt) {
-							}
-						})//autoComplete
-						
-		$(".pixel-radio").on("click",function(){
-			var gen = $("input[name='gen']:checked").val();
-			 $.ajax({
-				 url: "${pageContext.request.contextPath}/Ajax/audioBookCategoryAjax.do",
-				type: "GET",
-				data : {gen : gen},
-				success : function(data){ //연결성공
-				$("#row").html(data);
-				}//success
-			});//ajax
-		});//function
-	});
+$(function() {
+	$("#testInput").autocomplete({
+						source : function(request, response) {
+							$.ajax({
+									url : "${pageContext.request.contextPath}/Ajax/audioBookSearchAjax.do",
+									type : "GET",
+									dataType : "json",
+									data : {data : $("#testInput").val()}, // 검색 키워드
+									success : function(data) { // 성공
+										response($.map(data, function(item) {
+												$('#hidden').val(item.result);
+												$('#realHidden').val(item.title);
+												return {
+													label : item.title, //목록에 표시되는 값
+													value : item.title, //선택 시 input창에 표시되는 값	
+													idx : item.testIdx
+												};
+										})); //response
+									},//success
+									error : function() { //실패
+										alert("통신에 실패했습니다.");
+										}
+									});//ajax
+						},//source: function
+						minLength : 1,
+						autoFocus : false,
+						select : function(evt, ui) {
+							//$('#hidden').html(ui.item.value);
+							//var v = ui.item.label;
+							//var arr = v.split('/');
+							//$('#testInput').val(arr[0]);
+							//$('#hidden').val(arr[1]);
+							console.log("전체 data: " + JSON.stringify(ui));
+							console.log("db Index : " + ui.item.idx);
+							console.log("검색 데이터 : " + ui.item.value);
+						},
+						focus : function(evt, ui) {
+							return false;
+						},
+						close : function(evt) {
+						}
+					})//autoComplete
+	gopage(1); //시작하자마자 전체불러오기용
 	
-	function imgClick(book_no) {
-		if (book_no != null) {
-			if (confirm("해당 책 상세페이지로 이동하시겠습니까?")) {
-				location.href = "${pageContext.request.contextPath}/audioBookDetail.do?book_no="+book_no;
-			}
+	
+	$(".pixel-radio").on("click",function(){
+		gopage(1);
+	});//end of on & function
+	imgClick();
+
+	
+});
+	
+function gopage(p){
+	var gen = $("input[name='gen']:checked").val();
+	$.ajax({
+		url: "${pageContext.request.contextPath}/Ajax/audioBookCategoryAjax.do",
+		type: "GET",
+		data : {gen : gen},
+		success : function(data){ //연결성공
+		$("#row").html(data);
+		}//success
+	});//ajax
+} //function
+	
+	
+	
+function imgClick(book_no) {
+	if (book_no != null) {
+		if (confirm("해당 책 상세페이지로 이동하시겠습니까?")) {
+			location.href = "${pageContext.request.contextPath}/audioBookDetail.do?book_no="+book_no;
 		}
 	}
+}
 </script>
 
 
