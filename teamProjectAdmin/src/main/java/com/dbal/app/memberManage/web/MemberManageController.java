a5115a1994171fae3bac9d8028680ca39a01b95b
chaebin <ravis_toy@naver.com>
2020-10-07 11:49:25 +0900

    d

--- teamProjectAdmin/src/main/java/com/dbal/app/memberManage/web/MemberManageController.java
+++ teamProjectAdmin/src/main/java/com/dbal/app/memberManage/web/MemberManageController.java
@@ -1,5 +1,8 @@
 package com.dbal.app.memberManage.web;
 
+import java.util.HashMap;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.dbal.app.books.Books;
 import com.dbal.app.memberManage.Member;
 import com.dbal.app.memberManage.service.MemberManageService;
+
 
 @Controller
 public class MemberManageController {
@@ -33,21 +39,37 @@ public class MemberManageController {
 	}
 	//수정 처리
 	@RequestMapping(value = "/memberManageModify.ad", method = RequestMethod.POST)
-	public String memberManageInsert(Member memberVO, Model model) {
+	public String memberManageInsert(Member memberVO) {
 		memberManageService.update(memberVO);
 		return "redirect:/memberManageMain.ad";
 	}
 	
-	@RequestMapping(value="/memberManageDelete.ad", method = RequestMethod.POST)
+	@RequestMapping(value="/memberManageDelete.ad", method = RequestMethod.GET)
 	public String memberManageDelete(Member memberVO, HttpServletRequest request ) {
 		String member_no = request.getParameter("member_no");
-	//	memberVO.
+		memberVO.setMember_no(member_no);
 		memberManageService.delete(memberVO);
 		return "redirect:/memberManageMain.ad"; 
 	}
 	
 	
 	//그래프1
+	@RequestMapping(value="/viewChart.ad" )
+	@ResponseBody
+	public List<Books> viewChart(Books book) {
+		return memberManageService.selectViews();
+	}
 	// 그래프2
+	@RequestMapping(value="/genderChart.ad" )
+	@ResponseBody
+	public List<HashMap<String, Object>> getGenderCnt(){
+		return memberManageService.getGenderCnt();
+	}
 	// 그래프3
+	@RequestMapping(value="/ticketChart.ad")
+	@ResponseBody
+	public List<HashMap<String, Object>> getTicketCnt(){
+		return memberManageService.getTicketCnt();
+	}
+	
 }
