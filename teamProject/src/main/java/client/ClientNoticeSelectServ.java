82ffd1a67bf8d8b9f60717565baaca14c91d9de3
'monya-9' <'lee99714@naver.com'>
2020-10-12 14:31:56 +0900

    sfdsdsf

--- teamProject/src/main/java/client/ClientNoticeSelectServ.java
+++ teamProject/src/main/java/client/ClientNoticeSelectServ.java
@@ -13,12 +13,15 @@ public class ClientNoticeSelectServ implements Controller {
 
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Notice notice = new Notice();
+		
+		notice.setNotice_no((String)request.getSession().getAttribute("notice_no"));
+		notice.setNotice_no(request.getParameter("notice_no"));
 		notice.setNotice_title(request.getParameter("notice_title"));
-//		notice.setNotice_date(request.getParameter("notice_date"));
+		notice.setNotice_date(request.getParameter("notice_date"));
 		notice.setNotice_content(request.getParameter("notice_content"));
 		
 		NoticeDAO dao = new NoticeDAO();
-		dao.selectAll(notice);
+		dao.selectOne(notice);
 		
 		request.getRequestDispatcher("client/clientNoticeSelect.jsp").forward(request, response);
 		
