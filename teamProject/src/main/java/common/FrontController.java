9c8ede719fe1606af8805fddbd7bda81069fdfb7
admin <admin@YD03-24>
2020-10-12 19:48:20 +0900

    a

--- teamProject/src/main/java/common/FrontController.java
+++ teamProject/src/main/java/common/FrontController.java
@@ -203,7 +203,13 @@ public class FrontController extends HttpServlet {
 		list.put("/audioBookMarkInsert.do", new ebook.AudioBookMarkInsertServ());
 		list.put("/audioBookMarkDelete.do", new ebook.AudioBookMarkDeleteServ());
 		list.put("/Ajax/eBookReviewDeclaration.do", new ebook.EBookReviewDeclaration());
-	}ervletResponse response)
+	}
+	
+	public FrontController() {
+		super();
+	}
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding(charset);
 		String uri = request.getRequestURI(); // frontWeb/memberInsert.do
