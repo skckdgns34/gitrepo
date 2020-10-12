464fa88eaa3c497ab9d3dfb8534f1340f8689354
'khb3661' <'khb3661@naver.com'>
2020-10-12 12:07:24 +0900

    aaaa

--- teamProject/src/main/java/client/ClientHopeBookWriteServ.java
+++ teamProject/src/main/java/client/ClientHopeBookWriteServ.java
@@ -6,7 +6,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.beanutils.BeanUtils;
+
 import common.Controller;
+import vo.Hopebooks;
 
 public class ClientHopeBookWriteServ implements Controller
 {
@@ -14,7 +17,19 @@ public class ClientHopeBookWriteServ implements Controller
 	public void execute(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException
 	{
-		// TODO Auto-generated method stub
+		Hopebooks hopebooks = new Hopebooks();
+
+		try {
+			BeanUtils.copyProperties(hopebooks, request.getParameterMap());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		String member = (String) request.getSession().getAttribute("member_no");
+		hopebooks.setMember_no(member);
+		HopeBookDAO.getinstance().insert(hopebooks);
+
+		request.getRequestDispatcher("clientHopeBook.do").forward(request, response);
 
 	}
 
---
+++ teamProjectAdmin/src/main/java/com/dbal/app/memberManage/Hopebooks.java
@@ -0,0 +1,12 @@
+package com.dbal.app.memberManage;
+
+import lombok.Data;
+
+@Data
+public class Hopebooks {
+	private String hopebooks_no;
+	private String title;
+	private String writer;
+	private String member_no;
+	private String company;
+}
