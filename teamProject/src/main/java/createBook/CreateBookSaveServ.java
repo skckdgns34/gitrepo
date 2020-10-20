c254519dfc8a36e5944ac630b50942c411200d93
admin <admin@YD03-24>
2020-10-20 16:02:38 +0900

    zasd

--- teamProject/src/main/java/createBook/CreateBookSaveServ.java
+++ teamProject/src/main/java/createBook/CreateBookSaveServ.java
@@ -1,14 +1,18 @@
 package createBook;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import org.apache.commons.beanutils.BeanUtils;
 
+import bookManage.FileRenamePolicy;
 import common.Controller;
 import ebook.CommonDAO;
 import vo.Common;
@@ -26,29 +30,37 @@ public class CreateBookSaveServ  implements Controller {
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
+		
+		Part part = request.getPart("image_uri");
+		String book_img = getFilename(part);
+		String path = "c:/전자도서관/책표지";
+		System.out.println(path);
+		//파일명 중복체크
+		File renameFile = FileRenamePolicy.rename(new File(path, book_img));
+		part.write(path +"/" +renameFile.getName());
+		if(renameFile.getName().equals("책표지1")) {
+			book.setImage_uri(null);
+		}else {
+			book.setImage_uri(renameFile.getName());
+		}
+		
 		String chapter = book.getChapter();
 		book.setMember_no(member_no);
 		book.setMy_contents(contents);
-		
-		
-				
 		
 		if(chapter!=null && !chapter.equals("")) {
 			CreateBookDAO.getInstance().updateUserBookDetail(book);
 		}else{
 			CreateBookDAO.getInstance().saveUserBook(book);
 		}
-		
-		ArrayList<Common> genreList=  CommonDAO.getInstance().selectAllGenre();
-		ArrayList<Mywriting> chapterList = CreateBookDAO.getInstance().selectAllChapter(member_no, book.getMy_title());
-		
-		
-		request.setAttribute("genreList", genreList);
-		request.setAttribute("chapterList", chapterList);
-		request.setAttribute("title",book.getMy_title());
-		request.setAttribute("intro",book.getMy_introduction());
-		request.setAttribute("summary",book.getMy_summary());
-		request.setAttribute("my_genre",book.getGenre());
-		request.getRequestDispatcher("/createBook/createBookWrite.jsp").forward(request, response);
+		response.sendRedirect("createBook.do?title="+book.getMy_title());
+	}
+	private String getFilename(Part part) throws UnsupportedEncodingException {
+		for (String cd : part.getHeader("Content-Disposition").split(";")) {
+			if (cd.trim().startsWith("filename")) {
+				return cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+			}
+		}
+		return null;
 	}
 }
