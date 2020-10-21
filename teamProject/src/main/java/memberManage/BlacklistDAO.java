8a5730023e1a715f0b30c7e9818de7514c989666
admin <admin@YD03-24>
2020-10-21 16:24:58 +0900

    a

--- teamProject/src/main/java/memberManage/BlacklistDAO.java
+++ teamProject/src/main/java/memberManage/BlacklistDAO.java
@@ -63,7 +63,38 @@ public class BlacklistDAO {
 		}
 		return list;
 	}
-
+	public Blacklist selectM_no(Blacklist black) {
+		Blacklist resultVO = null;
+		ResultSet rs = null;
+		try {
+			conn = ConnectionManager.getConnnect();
+			String sql = " SELECT BLACKLIST_NO, BLACKLIST_REASON, MEMBER_NO, LIMIT_DATE"
+					+ " FROM BLACKLIST"
+					+ " WHERE MEMBER_NO=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, black.getMember_no());
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				resultVO = new Blacklist();
+				resultVO.setBlacklist_no(rs.getString("blacklist_no"));
+				resultVO.setBlacklist_reason(rs.getString("blacklist_reason"));
+				resultVO.setMember_no(rs.getString("member_no"));
+				resultVO.setLimit_date(rs.getString("limit_date"));
+				
+			} else {
+				System.out.println("no data");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionManager.close(rs, pstmt, conn);
+		}
+		return resultVO;
+	}
+	
+	
+	
+	
 	// 단건조회
 	public Blacklist selectOne(Blacklist black) {
 		Blacklist resultVO = null;
